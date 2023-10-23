c09e46ea321f07faf873fdb758b3cac330e1bf7e
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-10-23 17:18:17 +0800

    ⚙️ Chore: Update Swift Package Config

--- Package.swift
+++ Package.swift
@@ -1,7 +1,12 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
 let package = Package(
   name: "SwiftUtilities",
   platforms: [
-    .iOS(.v11),
+    .iOS(.v12),
   ],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
