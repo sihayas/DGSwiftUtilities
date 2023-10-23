835be19a39c246c82cbd8aed802edb18b06687c6
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-10-23 17:02:12 +0800

    🆕 Add: Ex - Add Example Project for SPM

---
+++ Example/SwiftUtilitiesExample/SwiftUtilitiesExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUtilitiesExample
+//
+//  Created by Dominic Go on 10/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
