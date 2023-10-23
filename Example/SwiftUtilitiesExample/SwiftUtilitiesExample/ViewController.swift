0359e49fc1c722f2b6d00e734d238fb46958360d
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-10-23 17:13:03 +0800

    💫 Update: Ex - Remove Storyboard Usage

--- Example/SwiftUtilitiesExample/SwiftUtilitiesExample/ViewController.swift
+++ Example/SwiftUtilitiesExample/SwiftUtilitiesExample/ViewController.swift
@@ -10,10 +10,8 @@ import UIKit
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
-
-
-}
+    super.viewDidLoad();
+    self.view.backgroundColor = .white;
+  };
+};
 
