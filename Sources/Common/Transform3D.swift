5b7cad454497b2abc6e5e6b7435a5df89c5d98f7
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-11-22 17:13:09 +0800

    💄 Gloss: Add Comment

--- Sources/Common/Transform3D.swift
+++ Sources/Common/Transform3D.swift
@@ -8,6 +8,17 @@
 import QuartzCore
 
 
+//  CGAffineTransform
+//
+//  | a  b  0 |
+//  | c  d  0 | =
+//  | tx ty 1 |
+//
+// |------------------ CGAffineTransformComponents ----------------|
+// | sx 0   0 |   | 1   0  0 |   |  cos(t)  sin(t) 0 |   | 1  0  0 |
+// | 0  sy  0 | * | sh  1  0 | * | -sin(t)  cos(t) 0 | * | 0  1  0 |
+// | 0  0   1 |   | 0   0  1 |   |  0       0      1 |   | tx ty 1 |
+//    scale           shear            rotation          translation
 
 /// [m11, m12, m13, m14]
 /// [m21, m22, m23, m24]
