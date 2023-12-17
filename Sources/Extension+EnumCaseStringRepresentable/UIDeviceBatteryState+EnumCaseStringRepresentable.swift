25c4268b1c724f907a3fee2c670de63a5389add8
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-12-17 20:45:52 +0800

    ⭐️ Impl: Add `UIDeviceBatteryState+EnumCaseStringRepresentable`

---
+++ Sources/Extension+EnumCaseStringRepresentable/UIDeviceBatteryState+EnumCaseStringRepresentable.swift
@@ -0,0 +1,35 @@
+//
+//  UIDeviceBatteryState+EnumCaseStringRepresentable.swift
+//
+//
+//  Created by Dominic Go on 12/17/23.
+//
+
+import UIKit
+
+
+extension UIDevice.BatteryState: EnumCaseStringRepresentable, CustomStringConvertible {
+  
+  public var caseString: String {
+    switch self {
+      case .unknown:
+        return "unknown";
+        
+      case .unplugged:
+        return "unplugged";
+        
+      case .charging:
+        return "charging";
+        
+      case .full:
+        return "full";
+        
+      @unknown default:
+        #if DEBUG
+        print("Runtime Warning - Not implemented -", #file);
+        #endif
+        
+        return "";
+    };
+  };
+};
