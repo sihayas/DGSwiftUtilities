94a6c5cb31b585b7ded27308f826d51dca101e88
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-12-17 20:34:15 +0800

    ⭐️ Impl: Add `UIUserInterfaceIdiom+CaseIterable`

---
+++ Sources/Extension+CaseIterable/UIUserInterfaceIdiom+CaseIterable.swift
@@ -0,0 +1,35 @@
+//
+//  UIUserInterfaceIdiom+CaseIterable.swift
+//  
+//
+//  Created by Dominic Go on 12/17/23.
+//
+
+import UIKit
+
+extension UIUserInterfaceIdiom: CaseIterable {
+
+  public static var allCases: [UIUserInterfaceIdiom] {
+    var cases: [Self] = [
+      .unspecified,
+    ];
+    
+    if #available(iOS 3.2, *) {
+      cases.append(.phone);
+      cases.append(.pad);
+    };
+
+    
+    if #available(iOS 9.0, *) {
+      cases.append(.tv);
+      cases.append(.carPlay);
+    };
+    
+    if #available(iOS 14.0, *) {
+      cases.append(.mac);
+    };
+    
+    return cases;
+  };
+};
+
