46e075729a01ff6e6f89eb6fafa5d8fbe7508a95
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-12-17 21:33:24 +0800

    ⭐️ Impl: Add `UIDeviceOrientation+EnumCaseStringRepresentable`

---
+++ Sources/Extension+EnumCaseStringRepresentable/UIDeviceOrientation+EnumCaseStringRepresentable.swift
@@ -0,0 +1,43 @@
+//
+//  UIDeviceOrientation+EnumCaseStringRepresentable.swift
+//
+//
+//  Created by Dominic Go on 12/17/23.
+//
+
+import UIKit
+
+extension UIDeviceOrientation: EnumCaseStringRepresentable {
+
+  public var caseString: String {
+    switch self {
+      case .unknown:
+        return "unknown";
+        
+      case .portrait:
+        return "portrait";
+        
+      case .portraitUpsideDown:
+        return "portraitUpsideDown";
+        
+      case .landscapeLeft:
+        return "landscapeLeft";
+        
+      case .landscapeRight:
+        return "landscapeRight";
+        
+      case .faceUp:
+        return "faceUp";
+        
+      case .faceDown:
+        return "faceDown";
+        
+      @unknown default:
+        #if DEBUG
+        print("Runtime Warning - Not implemented -", #file);
+        #endif
+        
+        return Self.unknown.caseString;
+    }
+  };
+};
