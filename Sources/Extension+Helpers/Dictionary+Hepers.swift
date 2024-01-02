683cd5a81e73148b7ddeae634710a32953febdc6
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-01-02 14:17:19 +0800

    💫 Update: `Dictionary+Hepers`

--- Sources/Extension+Helpers/Dictionary+Hepers.swift
+++ Sources/Extension+Helpers/Dictionary+Hepers.swift
@@ -5,7 +5,7 @@
 //  Created by Dominic Go on 12/22/23.
 //
 
-import Foundation
+import UIKit
 
 // TODO: Move to `DGSwiftUtilities`
 public extension Dictionary where Key == String {
@@ -65,6 +65,67 @@ public extension Dictionary where Key == String {
     );
     
     return try T.init(fromString: dictValue);
+  };
+  
+  func getValueFromDictionary<T: OptionSet & InitializableFromString>(
+    forKey key: String,
+    type: T.Type = T.self
+  ) throws -> T {
+  
+    let stringValues = try self.getValueFromDictionary(
+      forKey: "modalMaskedCorners",
+      type: [String].self
+    );
+    
+    var optionSets = stringValues.compactMap {
+      try? T.init(fromString: $0);
+    };
+    
+    guard let optionSetItem = optionSets.popLast() else {
+      throw GenericError(
+        errorCode: .unexpectedNilValue,
+        description: "array of optionSet values is 0",
+        extraDebugValues: [
+          "key": key
+        ]
+      );
+    };
+    
+    return optionSets.reduce(optionSetItem) {
+      $0.union($1);
+    };
+  };
+  
+  func getColorFromDictionary(forKey key: String) throws -> UIColor {
+    let stringValue = try? self.getValueFromDictionary(
+      forKey: key,
+      type: String.self
+    );
+    
+    if let stringValue = stringValue,
+       let color = UIColor(rgbString: stringValue) {
+      
+      return color;
+    };
+    
+    let dictValue = try? self.getValueFromDictionary(
+      forKey: key,
+      type: NSDictionary.self
+    );
+    
+    if let dictValue = dictValue,
+       let color = UIColor(dynamicDict: dictValue) {
+      
+      return color;
+    };
+    
+    throw GenericError(
+      errorCode: .unexpectedNilValue,
+      description: "Unable to get color from dictionary for key",
+      extraDebugValues: [
+        "key": key
+      ]
+    );
   };
   
   func getEnumFromDictionary<T: RawRepresentable<String>>(
