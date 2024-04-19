926cfc5059a49e28b56ab2f185e6063bc590cf41
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-04-19 14:52:03 +0800

    💫 Update: `PrivateObjectWrapper`

--- Sources/Common/ObjectWrapper/PrivateObjectWrapper.swift
+++ Sources/Common/ObjectWrapper/PrivateObjectWrapper.swift
@@ -16,13 +16,22 @@ open class PrivateObjectWrapper<
   EncodedString
 > {
 
+  public static var className: String? {
+    EncodedString.className.decodedString;
+  };
+  
+  public static var classType: AnyClass? {
+    guard let className = Self.className else { return nil };
+    return NSClassFromString(className);
+  };
+
   public override init?(
     objectToWrap sourceObject: AnyObject,
     shouldRetainObject: Bool = false
   ){
     
     guard let sourceObject = sourceObject as? NSObject,
-          let className = EncodedString.className.decodedString,
+          let className = Self.className,
           sourceObject.className == className
     else { return nil };
     
