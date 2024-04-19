d0406e57d6916da15a8b22a290c4366203f77ba1
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-04-19 14:51:49 +0800

    💫 Update: `SwizzlingHelpers`

--- Sources/Helpers/SwizzlingHelpers.swift
+++ Sources/Helpers/SwizzlingHelpers.swift
@@ -27,6 +27,34 @@ public class SwizzlingHelpers {
           
     method_exchangeImplementations(defaultInstance, newInstance);
   };
+  
+  @discardableResult
+  public static func swizzleWithBlock<T>(
+    impMethodType: T.Type,
+    forClass `class`: AnyClass,
+    withSelector selector: Selector,
+    newImpMaker: @escaping (
+      _ originalImp: T,
+      _ selector: Selector
+    ) -> Any
+  ) -> IMP? {
+  
+    // Retrieve the class method/IMP that matches the selector for the
+    // given type.
+    let originalMethod = class_getInstanceMethod(`class`, selector);
+    guard let originalMethod = originalMethod else { return nil };
+    
+    /// An `IMP` is just a C function pointer where the first two args are
+    /// `self` and `_cmd`.
+    let originalImp = method_getImplementation(originalMethod);
+    let originalImpFunc = unsafeBitCast(originalImp, to: T.self);
+    
+    let newImpBlock = newImpMaker(originalImpFunc, selector);
+    let newImp = imp_implementationWithBlock(newImpBlock);
+
+    // Swizzle - Replace `originalImpFunc` w/ `newImp`
+    return method_setImplementation(originalMethod, newImp);
+  };
 
   @discardableResult
   public static func swizzleWithBlock<T>(
@@ -57,7 +85,7 @@ public class SwizzlingHelpers {
   };
 
   @discardableResult
-  static func swizzleHitTest<T, U>(
+  public static func swizzleHitTest<T, U>(
     /// `UIView.hitTest(_:with:)` or:
     /// `func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView?`
     ///
@@ -78,6 +106,33 @@ public class SwizzlingHelpers {
     return Self.swizzleWithBlock(
       impMethodType: impMethodType,
       forObject: view,
+      withSelector: selector,
+      newImpMaker: hitTestBlockMaker
+    );
+  };
+  
+  @discardableResult
+  public static func swizzleHitTest<T, U>(
+    /// `UIView.hitTest(_:with:)` or:
+    /// `func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView?`
+    ///
+    impMethodType: T.Type =
+      (@convention(c) (Any, Selector, CGPoint, UIEvent?) -> UIView?).self,
+      
+    impBlockType: U.Type =
+      (@convention(block) (Any, CGPoint, UIEvent?) -> UIView?).self,
+      
+    forClass `class`: AnyClass,
+    hitTestBlockMaker: @escaping (
+      _ originalImp: T,
+      _ selector: Selector
+    ) -> U
+  ) -> IMP? {
+    let selector = #selector(UIView.hitTest(_:with:));
+    
+    return Self.swizzleWithBlock(
+      impMethodType: impMethodType,
+      forClass: `class`,
       withSelector: selector,
       newImpMaker: hitTestBlockMaker
     );
