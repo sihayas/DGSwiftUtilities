13db6b4527478c0ce2a55f4e90ae734ae6b76b1a
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-06-21 10:53:08 +0800

    💫 Update: `ObjectWrapper`

--- Sources/Common/ObjectWrapper/ObjectWrapper.swift
+++ Sources/Common/ObjectWrapper/ObjectWrapper.swift
@@ -13,11 +13,20 @@ open class ObjectWrapper<
   EncodedString: HashedStringDecodable
 > {
 
+  // MARK: - Properties
+  // ------------------
+
   public var objectWrapper: ObjectContainer<WrapperType>;
+  
+  // MARK: - Computed Properties
+  // ---------------------------
   
   public var wrappedObject: WrapperType? {
     self.objectWrapper.object;
-  }; 
+  };
+  
+  // MARK: - Init
+  // ------------
   
   public init?(
     objectToWrap sourceObject: AnyObject,
@@ -31,6 +40,9 @@ open class ObjectWrapper<
       shouldRetainObject: shouldRetainObject
     );
   };
+  
+  // MARK: - Methods
+  // ---------------
   
   @discardableResult
   public func performSelector<T>(
@@ -62,5 +74,58 @@ open class ObjectWrapper<
     #if DEBUG
     print(self.wrappedObject.debugDescription);
     #endif
+  };
+  
+  // MARK: Class Methods
+  // -------------------
+  
+  @discardableResult
+  public func getValue<T>(
+    forHashedString encodedString: EncodedString,
+    type: T.Type = Any.self
+  ) throws -> T? {
+    guard let wrappedObject = self.wrappedObject as? NSObject,
+          let key = encodedString.decodedString
+    else { return nil };
+    
+    let value = wrappedObject.value(forKey: key);
+    return value as? T;
+  };
+  
+  public func setValue<T>(
+    forHashedString encodedString: EncodedString,
+    value: T?
+  ) throws {
+    guard let wrappedObject = self.wrappedObject as? NSObject,
+          let key = encodedString.decodedString
+    else { return };
+    
+    wrappedObject.setValue(value, forKey: key);
+  };
+};
+
+// MARK: - Class Methods
+// ---------------------
+
+extension ObjectWrapper {
+
+  @discardableResult
+  public static func performSelector<T>(
+    usingHashedString encodedString: EncodedString,
+    forObject object: AnyObject,
+    withArg1 arg1: Any? = nil,
+    withArg2 arg2: Any? = nil,
+    type: T.Type = Any.self,
+    shouldRetainValue: Bool = false
+  ) throws -> T? {
+  
+    return try ObjectWrapperHelpers.performSelector(
+      forObject: object,
+      selectorFromHashedString: encodedString,
+      withArg1: arg1,
+      withArg2: arg2,
+      type: type,
+      shouldRetainValue: shouldRetainValue
+    );
   };
 };
