f37dad28fb2224ce69758f98f037f53239d5a895
Dominic Go <dominic@dominicgo.dev>
2024-07-10 21:14:52 +0800

    ⭐️ Impl: `Array+ComparableHelpers`

---
+++ Sources/Extension+Helpers/Array+ComparableHelpers.swift
@@ -0,0 +1,26 @@
+//
+//  Array+ComparableHelpers.swift
+//  
+//
+//  Created by Dominic Go on 7/10/24.
+//
+
+import Foundation
+
+
+public extension Array where Element: Comparable  {
+  
+  var indexedMin: IndexElementPair? {
+    self.enumerated().reduce(into: nil) {
+      guard let current = $0 else {
+        $0 = ($1.offset, $1.element);
+        return;
+      };
+      
+      if current.value.isLessThan(to: $1.element) {
+        $0 = ($1.offset, $1.element);
+      };
+    };
+  };
+};
+
