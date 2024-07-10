094756160187387c9f10bdfdf9b9f7a411f5f327
Dominic Go <dominic@dominicgo.dev>
2024-07-10 21:34:34 +0800

    ⭐️ Impl: `Array+ComparableHelpers`

--- Sources/Extension+Helpers/Array+ComparableHelpers.swift
+++ Sources/Extension+Helpers/Array+ComparableHelpers.swift
@@ -22,5 +22,18 @@ public extension Array where Element: Comparable  {
       };
     };
   };
+  
+  var indexedMax: IndexElementPair? {
+    self.enumerated().reduce(into: nil) {
+      guard let current = $0 else {
+        $0 = ($1.offset, $1.element);
+        return;
+      };
+      
+      if current.value.isGreaterThan(to: $1.element) {
+        $0 = ($1.offset, $1.element);
+      };
+    };
+  };
 };
 
