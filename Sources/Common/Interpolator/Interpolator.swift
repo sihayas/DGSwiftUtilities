3f119c2a359242ffb187aa3220e5445c51f25f6b
Dominic Go <dominic@dominicgo.dev>
2024-07-13 10:53:01 +0800

    ⭐️ Impl: Custom Input Range For `Interpolator`

--- Sources/Common/Interpolator/Interpolator.swift
+++ Sources/Common/Interpolator/Interpolator.swift
@@ -9,27 +9,83 @@ import Foundation
 
 public struct Interpolator {
   
-  public var valueStart: CGFloat;
-  public var valueEnd: CGFloat;
+  public var inputValueStart: CGFloat = 0;
+  public var inputValueEnd: CGFloat = 1;
   
-  func lerp(
-    percent: CGFloat,
-    easing: InterpolationEasing? = nil
-  ) -> CGFloat {
+  public var outputValueStart: CGFloat;
+  public var outputValueEnd: CGFloat;
+  
+  public var easing: InterpolationEasing;
+  private var _hasCustomInputRange = false;
+  
+  // MARK: - Init
+  // ------------
+  
+  public init(
+    valueStart: CGFloat,
+    valueEnd: CGFloat,
+    easing: InterpolationEasing = .linear
+  ) {
+    self.outputValueStart = valueStart;
+    self.outputValueEnd = valueEnd;
+    self.easing = easing;
+  };
+  
+  public init(
+    inputValueStart: CGFloat,
+    inputValueEnd: CGFloat,
+    outputValueStart: CGFloat,
+    outputValueEnd: CGFloat,
+    easing: InterpolationEasing = .linear
+  ) {
+    self.inputValueStart = inputValueStart;
+    self.inputValueEnd = inputValueEnd;
+    self.outputValueStart = outputValueStart;
+    self.outputValueEnd = outputValueEnd;
+    self.easing = easing;
     
-    if let easing = easing {
-      return Self.lerp(
-        valueStart: self.valueStart,
-        valueEnd: self.valueEnd,
-        percent: percent,
-        easing: easing
+    self._hasCustomInputRange = true;
+  };
+  
+  // MARK: - Functions
+  // -----------------
+  
+  public func interpolate(
+    percent: CGFloat,
+    easingOverride: InterpolationEasing? = nil
+  ) -> CGFloat {
+  
+    if self._hasCustomInputRange {
+      return RangeInterpolator.interpolate(
+        relativePercent: percent,
+        inputValueStart: inputValueStart,
+        inputValueEnd: inputValueEnd,
+        outputValueStart: outputValueStart,
+        outputValueEnd: outputValueEnd,
+        easing: easingOverride ?? self.easing
       );
     };
     
     return Self.lerp(
-      valueStart: self.valueStart,
-      valueEnd: self.valueEnd,
-      percent: percent
+      valueStart: self.outputValueStart,
+      valueEnd: self.outputValueEnd,
+      percent: percent,
+      easing: easingOverride ?? self.easing
+    );
+  };
+  
+  public func interpolate(
+    inputValue: CGFloat,
+    easingOverride: InterpolationEasing? = nil
+  ) -> CGFloat {
+  
+    return RangeInterpolator.interpolate(
+      inputValue: inputValue,
+      inputValueStart: self.inputValueStart,
+      inputValueEnd: self.inputValueEnd,
+      outputValueStart: self.outputValueStart,
+      outputValueEnd: self.outputValueEnd,
+      easing: easingOverride ?? self.easing
     );
   };
 };
