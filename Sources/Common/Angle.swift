0a3fa85c414c9ba1ed6eb9a1dfdcb3960c1d38ec
Dominic Go <dominic@dominicgo.dev>
2024-07-24 06:25:01 +0800

    🛠 Refactor: Re-Write `Angle`

--- Sources/Common/Angle.swift
+++ Sources/Common/Angle.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  Angle.swift
 //  
 //
 //  Created by Dominic Go on 8/7/23.
@@ -8,17 +8,17 @@
 import Foundation
 
 
-public enum Angle<T: BinaryFloatingPoint>: Equatable {
-
-  case zero;
+public enum Angle<T: BinaryFloatingPoint>: Equatable, Comparable, EnumCaseStringRepresentable {
+  
   case radians(T);
   case degrees(T);
   
   public var radians: T {
+    guard self.rawValue == 0 else {
+      return 0;
+    };
+  
     switch self {
-      case .zero:
-        return 0;
-      
       case let .degrees(value):
         let pi = type(of: value).pi;
         return value * (pi / 180);
@@ -29,10 +29,11 @@ public enum Angle<T: BinaryFloatingPoint>: Equatable {
   };
   
   public var degrees: T {
-    switch self {
-      case .zero:
-        return 0;
+    guard self.rawValue == 0 else {
+      return 0;
+    };
     
+    switch self {
       case let .degrees(value):
         return value;
         
@@ -40,5 +41,83 @@ public enum Angle<T: BinaryFloatingPoint>: Equatable {
         let pi = type(of: value).pi;
         return value * (180 / pi);
     };
+  };
+  
+  public var rawValue: T {
+    get {
+      switch self {
+        case .zero:
+          return 0;
+          
+        case let .radians(value):
+          return value;
+          
+        case let .degrees(value):
+          return value;
+      };
+    }
+    set {
+      switch self {
+        case .zero:
+          self = .zero;
+          
+        case let .radians(value):
+          self = .radians(value);
+          
+        case let .degrees(value):
+          self = .degrees(value);
+      };
+    }
+  };
+  
+  public var caseString: String {
+    switch self {
+      case .radians:
+        return "radians";
+        
+      case .degrees:
+        return "degrees";
+    };
+  };
+};
+
+// MARK: - Angle+StaticAlias
+// -------------------------
+
+public extension Angle {
+
+  static var zero: Self {
+    .degrees(0);
+  };
+};
+
+// MARK: - Angle+StaticHelpers
+// ---------------------------
+
+public extension Angle {
+
+  static func normalizeToDegrees(_ a: Self, _ b: Self) -> (a: Self, b: Self){
+    switch (a, b){
+      case (.degrees, .radians):
+        return (a, .degrees(b.degrees));
+        
+      case (.radians, .degrees):
+        return (.degrees(a.degrees), b);
+        
+      case (.radians, .radians):
+        return (.degrees(a.degrees), .degrees(b.degrees));
+        
+      case (.degrees, .degrees):
+        return (a, b);
+    };
+  };
+  
+  static func normalizeToRadians(_ a: Self, _ b: Self) -> (a: Self, b: Self){
+    let (normalizedA, normalizedB) = Self.normalizeToDegrees(a, b);
+    
+    return (
+      .radians(normalizedA.radians),
+      .radians(normalizedB.radians)
+    );
   };
 };
