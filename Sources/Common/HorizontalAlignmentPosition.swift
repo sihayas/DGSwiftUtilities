39b2bbd0875541fb39b05caddbe63dddb14dd60b
cora type <coratype@gmail.com>
2025-02-02 14:24:57 -0500

    Added margin support

--- Sources/Common/HorizontalAlignmentPosition.swift
+++ Sources/Common/HorizontalAlignmentPosition.swift
@@ -1,6 +1,6 @@
 //
 //  HorizontalAlignmentPosition.swift
-//  
+//
 //
 //  Created by Dominic Go on 10/25/23.
 //
@@ -9,124 +9,125 @@ import UIKit
 
 // TODO: Move to `DGSwiftUtilities`
 public enum HorizontalAlignmentPosition: String {
-  case stretch;
-  case stretchTarget;
+    case stretch
+    case stretchTarget
   
-  case targetLeading;
-  case targetTrailing;
-  case targetCenter;
+    case targetLeading
+    case targetTrailing
+    case targetCenter
   
-  // MARK: - Computed Properties
-  // ---------------------------
-  
-  var shouldSetWidth: Bool {
-    switch self {
-      case .targetLeading, .targetTrailing, .targetCenter:
-        return true;
-      
-      default:
-        return false;
-    };
-  };
-  
-  // MARK: - Functions
-  // -----------------
-  
-  public func createHorizontalConstraints(
-    forView view: UIView,
-    attachingTo targetView: UIView,
-    enclosingView: UIView,
-    preferredWidth: CGFloat?,
-    marginLeading: CGFloat = 0,
-    marginTrailing: CGFloat = 0,
-    shouldPreferWidthAnchor: Bool = false
-  ) -> [NSLayoutConstraint] {
-  
-    var constraints: [NSLayoutConstraint?] = [];
-  
-    let widthAnchor: NSLayoutConstraint? = {
-      guard let preferredWidth = preferredWidth else { return nil };
-      
-      return view.widthAnchor.constraint(
-        equalToConstant: preferredWidth
-      );
-    }();
-  
-    switch self {
-      // A - pin to left
-      case .targetLeading:
-        constraints += [
-          widthAnchor,
-          view.leadingAnchor.constraint(
-            equalTo: targetView.leadingAnchor,
-            constant: marginLeading
-          ),
-        ];
-      
-      // B - pin to right
-      case .targetTrailing:
-        constraints += [
-          widthAnchor,
-          view.trailingAnchor.constraint(
-            equalTo: targetView.trailingAnchor,
-            constant: -marginTrailing
-          )
-        ];
-      
-      // C - pin to center
-      case .targetCenter:
-        constraints +=  [
-          widthAnchor,
-          view.centerXAnchor.constraint(
-            equalTo: targetView.centerXAnchor
-          ),
-        ];
-      
-      // D - match preview size
-      case .stretchTarget:
-        constraints += shouldPreferWidthAnchor ? [
-          view.centerXAnchor.constraint(
-            equalTo: targetView.centerXAnchor
-          ),
-          
-          view.widthAnchor.constraint(
-            equalToConstant: targetView.bounds.width
-          ),
-        ] : [
-          view.leadingAnchor.constraint(
-            equalTo: targetView.leadingAnchor,
-            constant: marginLeading
-          ),
-          
-          view.trailingAnchor.constraint(
-            equalTo: targetView.trailingAnchor,
-            constant: marginTrailing
-          ),
-        ];
-      
-      // E - stretch to edges of screen
-      case .stretch:
-        constraints += shouldPreferWidthAnchor ?  [
-          view.centerXAnchor.constraint(
-            equalTo: targetView.centerXAnchor
-          ),
-          
-          view.widthAnchor.constraint(
-            equalToConstant: enclosingView.bounds.width
-          ),
-        ] : [
-         view.leadingAnchor.constraint(
-           equalTo: enclosingView.leadingAnchor,
-           constant: marginLeading
-         ),
+    // MARK: - Computed Properties
 
-         view.trailingAnchor.constraint(
-           equalTo: enclosingView.trailingAnchor,
-           constant: marginTrailing
-         ),
-      ];
-    };
+    // ---------------------------
+  
+    var shouldSetWidth: Bool {
+        switch self {
+            case .targetLeading, .targetTrailing, .targetCenter:
+                return true
+      
+            default:
+                return false
+        }
+    }
+  
+    // MARK: - Functions
+
+    // -----------------
+  
+    public func createHorizontalConstraints(
+        forView view: UIView,
+        attachingTo targetView: UIView,
+        enclosingView: UIView,
+        preferredWidth: CGFloat?,
+        marginLeading: CGFloat,
+        marginTrailing: CGFloat,
+        shouldPreferWidthAnchor: Bool = false
+    ) -> [NSLayoutConstraint] {
+        var constraints: [NSLayoutConstraint?] = []
+  
+        let widthAnchor: NSLayoutConstraint? = {
+            guard let preferredWidth = preferredWidth else { return nil }
+      
+            return view.widthAnchor.constraint(
+                equalToConstant: preferredWidth
+            )
+        }()
+  
+        switch self {
+            // A - pin to left
+            case .targetLeading:
+                constraints += [
+                    widthAnchor,
+                    view.leadingAnchor.constraint(
+                        equalTo: targetView.leadingAnchor,
+                        constant: marginLeading
+                    ),
+                ]
+      
+            // B - pin to right
+            case .targetTrailing:
+                constraints += [
+                    widthAnchor,
+                    view.trailingAnchor.constraint(
+                        equalTo: targetView.trailingAnchor,
+                        constant: marginTrailing
+                    ),
+                ]
+      
+            // C - pin to center
+            case .targetCenter:
+                constraints += [
+                    widthAnchor,
+                    view.centerXAnchor.constraint(
+                        equalTo: targetView.centerXAnchor
+                    ),
+                ]
+      
+            // D - match preview size
+            case .stretchTarget:
+                constraints += shouldPreferWidthAnchor ? [
+                    view.centerXAnchor.constraint(
+                        equalTo: targetView.centerXAnchor
+                    ),
+          
+                    view.widthAnchor.constraint(
+                        equalToConstant: targetView.bounds.width
+                    ),
+                ] : [
+                    view.leadingAnchor.constraint(
+                        equalTo: targetView.leadingAnchor,
+                        constant: marginLeading
+                    ),
+          
+                    view.trailingAnchor.constraint(
+                        equalTo: targetView.trailingAnchor,
+                        constant: marginTrailing
+                    ),
+                ]
+      
+            // E - stretch to edges of screen
+            case .stretch:
+                constraints += shouldPreferWidthAnchor ? [
+                    view.centerXAnchor.constraint(
+                        equalTo: targetView.centerXAnchor
+                    ),
+          
+                    view.widthAnchor.constraint(
+                        equalToConstant: enclosingView.bounds.width
+                    ),
+                ] : [
+                    view.leadingAnchor.constraint(
+                        equalTo: enclosingView.leadingAnchor,
+                        constant: marginLeading
+                    ),
+
+                    view.trailingAnchor.constraint(
+                        equalTo: enclosingView.trailingAnchor,
+                        constant: marginTrailing
+                    ),
+                ]
+        }
     
-    return constraints.compactMap { $0 };
-  };
-};
+        return constraints.compactMap { $0 }
+    }
+}
